bd26c05acafbf2f23fcccad1ce7c7c1a805a950b
selclass <33724398+selclass@users.noreply.github.com>
2019-02-04 09:56:10 +0530

    Update PalandromNumber.java
    this is added in the github at first

--- src/main/java/progInrvQA/PalandromNumber.java
+++ src/main/java/progInrvQA/PalandromNumber.java
@@ -2,7 +2,7 @@ package progInrvQA;
 
 public class PalandromNumber {
 
-	
+// this is added in the github	
 	public static void isPalndrom(int num) {
 	// this is in github
 	// now i added one more in the W7
