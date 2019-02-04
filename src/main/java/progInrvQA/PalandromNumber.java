77a95af890d659d82ee50655d05582a0d54b77d7
anaadity <anaadity@WKWIN9870376.global.publicisgroupe.net>
2019-02-04 09:57:22 +0530

    this is commit in the w7

--- src/main/java/progInrvQA/PalandromNumber.java
+++ src/main/java/progInrvQA/PalandromNumber.java
@@ -5,10 +5,11 @@ public class PalandromNumber {
 	
 	public static void isPalndrom(int num) {
 	// this is in github
-	// now i added one more in the W7
-	
-		
+	// now i added one more in the W7		
 	}
+	
+	
+	// this is on line 12
 
 
 }
