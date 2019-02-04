f4188aaf35f4b339149ef29a76137ffae4331b3d
anaadity <anaadity@WKWIN9870376.global.publicisgroupe.net>
2019-02-04 10:00:28 +0530

    commit from w7

--- src/main/java/progInrvQA/PalandromNumber.java
+++ src/main/java/progInrvQA/PalandromNumber.java
@@ -2,7 +2,7 @@ package progInrvQA;
 
 public class PalandromNumber {
 
-// this is added in the github	
+// this is added in the github added	
 	public static void isPalndrom(int num) {
 	// this is in github
 	// now i added one more in the W7		
